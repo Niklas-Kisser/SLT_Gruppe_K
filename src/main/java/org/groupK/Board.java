53a89113526a87ebc780ad6cbd74f342be3caa11
Katharina Mandl <wi25b108@technikum-wien.at>
2026-06-22 21:52:13 +0200

    feat(board.java): Implemented basic board class with empty methods

--- src/main/java/org/groupK/Board.java
+++ src/main/java/org/groupK/Board.java
@@ -1,4 +1,31 @@
 package org.groupK;
 
 public class Board {
+    private char[][] cells ;
+
+
+    public Board() {
+        
+    }
+
+    public boolean isCellEmpty(int x, int y){
+        return true;
+    }
+
+    public void place(int x, int y, char marker){
+
+    }
+
+    public boolean isFull(){
+        return false;
+    }
+
+    public void clear(){
+
+    }
+
+    public void print(){
+
+    }
+
 }
