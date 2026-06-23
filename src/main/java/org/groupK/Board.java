592af3887957e772b27dffc85afa6e502ec7936c
Nikkey25 <niklas.kisser@gmx.at>
2026-06-23 14:03:09 +0200

    feat(Board.java): add move validation, clear and print

--- src/main/java/org/groupK/Board.java
+++ src/main/java/org/groupK/Board.java
@@ -1,31 +1,57 @@
 package org.groupK;
 
 public class Board {
-    private char[][] cells ;
-
+    private char[][] cells;
 
     public Board() {
-        
+        // Tic-Tac-Toe Feld mit einem 3x3 Feld
+        cells = new char[3][3];
+        clear();
     }
 
-    public boolean isCellEmpty(int x, int y){
+    public boolean isCellEmpty(int x, int y) {
+        // Prüfen, ob die Koordinaten im gültig sind
+        if (x < 0 || x > 2 || y < 0 || y > 2) {
+            return false;
+        }
+        // Wenn das Feld ein Leerzeichen ist,
+        return cells[x][y] == ' ' || cells[x][y] == '\u0000';
+    }
+
+    public void place(int x, int y, char marker) {
+        if (isCellEmpty(x, y)) {
+            cells[x][y] = marker;
+        }
+    }
+
+    public boolean isFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (cells[i][j] == ' ' || cells[i][j] == '\u0000') {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
-    public void place(int x, int y, char marker){
-
+    public void clear() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = ' ';
+            }
+        }
     }
 
-    public boolean isFull(){
-        return false;
+    public void print() {
+        System.out.println("_____");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(cells[i][j] + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("_____");
     }
-
-    public void clear(){
-
-    }
-
-    public void print(){
-
-    }
-
 }
